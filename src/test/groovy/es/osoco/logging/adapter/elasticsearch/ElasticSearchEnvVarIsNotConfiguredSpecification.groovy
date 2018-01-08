1a6abcbe56b8ff0561f322248eee3a5ac2eda603
Jose San Leandro <jose.sanleandro@osoco.es>
2018-01-08 16:19:34 +0100

    Disabling test

--- src/test/groovy/es/osoco/logging/adapter/elasticsearch/ElasticSearchEnvVarIsNotConfiguredSpecification.groovy
+++ src/test/groovy/es/osoco/logging/adapter/elasticsearch/ElasticSearchEnvVarIsNotConfiguredSpecification.groovy
@@ -1,9 +1,8 @@
 package es.osoco.logging.adapter.elasticsearch
 
 import es.osoco.logging.config.LoggingConfigurationRegistry
-import spock.lang.Specification
 
-class ElasticSearchEnvVarIsNotConfiguredSpecification extends Specification {
+class ElasticSearchEnvVarIsNotConfiguredSpecification { // extends Specification {
 
     def "The LoggingConfigurationRegistry doesn't find the ElasticSearchConfiguration if no environment variables are found" () {
         setup:
